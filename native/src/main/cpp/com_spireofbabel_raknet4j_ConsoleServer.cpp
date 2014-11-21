790b25e3e68bcc1a654d72076540b21c720ca6e4
Ben Carson <ben.carson@bigpond.com>
2014-11-21 22:39:00 +1100

    Fixed ConsoleServer.GetInstance and DestroyInstance implementation.

--- native/src/main/cpp/com_spireofbabel_raknet4j_ConsoleServer.cpp
+++ native/src/main/cpp/com_spireofbabel_raknet4j_ConsoleServer.cpp
@@ -5,18 +5,18 @@
 
 using namespace RakNet;
 
-/*JNIEXPORT jobject JNICALL Java_com_spireofbabel_raknet4j_ConsoleServer_nativeGetInstance
+JNIEXPORT jobject JNICALL Java_com_spireofbabel_raknet4j_ConsoleServer_nativeGetInstance
   (JNIEnv *env, jclass)
 {
     ConsoleServer *instance = ConsoleServer :: GetInstance();
 
-    return createHandleObject<ConsoleServer>(env, instance);
+    return createHandleObject(env, instance);
 }
 
 JNIEXPORT void JNICALL Java_com_spireofbabel_raknet4j_ConsoleServer_nativeDestroyInstance
   (JNIEnv *env, jclass, jobject handle)
 {
-    ConsoleServer *instance = getHandle<ConsoleServer>(env, handle);
+    ConsoleServer *instance = dereferenceHandle<ConsoleServer>(env, handle);
 
     ConsoleServer :: DestroyInstance(instance);
-}*/
+}
